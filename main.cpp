a7575e52b96d0bac11eb4c43cf2b993af50b5f9a
Mazen Elkashlan <mazenelkashlan34@gmail.com>
2026-03-21 23:01:38 +0200

    [HMS-09] implemented bookAppointment and addEmergency

--- main.cpp
+++ main.cpp
@@ -219,10 +219,51 @@ public:
         return doctorCounter++;
     }
     void admitPatient(int patientId, RoomType type);
-    void addEmergency(int patientId);
+
+    void addEmergency(int patientId){
+
+        emergencyQueue.push(patientId);
+        
+        cout << "Emergency added for patient " << patientId << "." << endl;
+    }
     int handleEmergency();
-    void bookAppointment(int doctorId, int patientId);
+
+    void bookAppointment(int doctorId, int patientId){
+        
+        bool flagDoctor=false;
+        bool flagPatient=false;
+        Doctor* foundDoctor = nullptr;
+
+        for (auto& i: doctors) {
+      	    if (i.getId()==doctorId){
+                flagDoctor=true;
+                foundDoctor = &i;
+            }
+        }
+
+        for (auto j: patients) {
+      	    if (j.getId()==patientId){
+                flagPatient=true;
+            }
+        }
+
+        if (!flagDoctor){
+            cout << "Doctor not found." << endl;
+        }
+
+        else if (!flagPatient)
+        {
+            cout << "Patient not found." << endl;
+        }
+
+        else{
+            foundDoctor->addAppointment(patientId);
+        }
+        
+
+    }
     void displayPatientInfo(int patientId);
+
     void displayDoctorInfo(int doctorId);
 };
 
